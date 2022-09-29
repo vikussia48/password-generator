29a5733017ba032a7ac9c1a4fd90e96ba05692a3
vikussia48 <viktorijax4@gmail.com>
2022-09-29 15:42:10 +0100

    Created an option to include symbols or not and also allowed to copy passwords to clipboard

--- index.js
+++ index.js
@@ -2,30 +2,45 @@
 
 // Additional features to add:
 // 1. Ability to set password length
-// 2. Add copy on click
-// 3. Toggle "symbols" and "numbers" on/off
 
 // Character array to use for the password
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
 "T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-"r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`",
-"!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",
-".","?","/"];
+"r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
+",","|",":",";","<",">",".","?","/"]
+
+let array = []
 let password1 = ""
 let password2 = ""
 let passwordEl1 = document.getElementById('password-el1')
 let passwordEl2 = document.getElementById('password-el2')
+let toggle = document.getElementById('switch')
 
 function generatePassword() {
+    if (toggle.checked) {
+        array = characters.concat(symbols)
+    } else {
+        array = characters
+    }
+
     for (let i = 0; i < 15; i += 1) {
-        password1 += characters[Math.floor(Math.random() * characters.length)]
-        password2 += characters[Math.floor(Math.random() * characters.length)]
+        password1 += array[Math.floor(Math.random() * array.length)]
+        password2 += array[Math.floor(Math.random() * array.length)]
     }
     passwordEl1.textContent = password1
+    passwordEl1.style.backgroundColor = "#E6DFF6"
     passwordEl2.textContent = password2
+    passwordEl2.style.backgroundColor = "#E6DFF6"
     password1 = ""
     password2 = ""
 }
 
+passwordEl1.addEventListener("click", function() {
+    navigator.clipboard.writeText(passwordEl1.textContent)
+})
 
+passwordEl2.addEventListener("click", function() {
+    navigator.clipboard.writeText(passwordEl2.textContent)
+})
